56fb845ed14b141bfd7b3c2644dc5b8bd746728f
BuildTools <unconfigured@null.spigotmc.org>
2020-04-30 15:25:17 +0100

    No u addition

--- newRetr0Bot.py
+++ newRetr0Bot.py
@@ -94,6 +94,15 @@ async def clear(ctx, number=5):
 async def kick(ctx, member : discord.Member, * , reason = None):
     await member.kick(reason=reason)
 
+@client.event
+async def on_message(message):
+    responses=["Still you", "ur dad", "It's not me, it's youuuuu.", "no u.", "uno reverse".]
+    if message.author==client.user:
+        return
+    if message.content == "no u" or message.content == "No u":
+        reply=random.choice(responses)
+        await ctx.send(reply)
+
 @client.command()
 @commands.has_permissions(ban_members=True)
 async def ban(ctx, member : discord.Member, * , reason = None):
